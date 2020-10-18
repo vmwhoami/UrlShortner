c870e7bf2af1ac469ec2dd5a04e1bb98b3e5b570
Vitalie Melnic <vmwhoami@gmail.com>
2020-10-18 16:15:52 +0300

    Add short url method

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -1,3 +1,7 @@
-class ShortenedUrls < ApplicationRecord
-  
+class ShortenedUrl < ApplicationRecord
+  validates :short_url, presence: true, uniqueness: { case_sensitive: false }
+
+  def random_code 
+    SecureRandom.base64 16
+  end
 end
